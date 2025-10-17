96809989c5b0b6e8f194bd73f4bf5deb9c57e530
Claudionei de Aguiar <cdaguiar23@gmail.com>
2025-10-17 18:21:58 -0300

    Corrige problema de CORS no AuthController adicionando @CrossOrigin

--- src/main/java/com/productmanager/produto/controller/AuthController.java
+++ src/main/java/com/productmanager/produto/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.productmanager.produto.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins = "*")
 @Tag(name = "Authentication", description = "Endpoints para autenticação de usuários")
 public class AuthController {
 
